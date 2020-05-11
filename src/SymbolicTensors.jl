b32395c3eca60c5159524fcc4611b7c796dcd0a7
Robbie Rosati <rjrosati@utexas.edu>
2020-05-11 15:25:14 -0500

    basic classes working! added deps too

--- src/SymbolicTensors.jl
+++ src/SymbolicTensors.jl
@@ -17,29 +17,46 @@ export TensorIndexType, tensor_indices, TensorIndex, TensorHead
 export TensorSymmetry
 #export @heads,@indices
 export @indices
+export tensor
 
 struct Tensor <: SymbolicObject end
 struct TensorHead <: SymbolicObject
-    __pyobject__::PyCall.PyObjct
+    __pyobject__::PyCall.PyObject
 end
 struct TensorIndex <: SymbolicObject
-    __pyobject__::PyCall.PyObjct
+    __pyobject__::PyCall.PyObject
 end
 struct TensorIndexType <: SymbolicObject
-    __pyobject__::PyCall.PyObjct
+    __pyobject__::PyCall.PyObject
+end
+struct TensorSymmetry <: SymbolicObject
+    __pyobject__::PyCall.PyObject
 end
 
-TensorIndex(s::TensorIndex) == s
-TensorIndex(name::AbstractString,tensor_index_type::TensorIndexType,is_up::Bool=true) = sympy.tensor.TensorIndex(name,tensor_index_type,is_up)
-TensorIndex(name::Symbol,tensor_index_type::TensorIndexType,is_up::Bool=true) = sympy.tensor.TensorIndex(name,tensor_index_type,is_up)
-#-(x::TensorIndex) = x.__neg__()
+const tensor = PyCall.PyNULL()
+const toperations = PyCall.PyNULL()
 
-TensorHead(s::TensorHead) == s
-TensorHead(name::AbstractString,index_types::AbstractArray{TensorIndexType},symmetry::TensorSymmetry) = sympy.tensor.TensorHead(name,index_types,symmetry)
-TensorHead(name::Symbol,index_types::AbstractArray{TensorIndexType},symmetry::TensorSymmetry) = sympy.tensor.TensorHead(name,index_types,symmetry)
+function __init__()
+    copy!(tensor,PyCall.pyimport_conda("sympy.tensor.tensor","sympy"))
+#    copy!(toperations,PyCall.pyimport_conda("sympy.tensor.toperations","sympy"))
+end
 
-TensorIndexType(name::AbstractString,dummy_name::AbstractString,dim::Int,metric_name::AbstractString) = sympy.tensor.TensorIndexType(name,dummy_name,dim,metric_name=metric_name)
-TensorIndexType(name::Symbol,dummy_name::Symbol,dim::Int,metric_name::Symbol) = sympy.tensor.TensorIndexType(string(name),string(dummy_name),dim,metric_name=string(metric_name))
+TensorIndex(s::TensorIndex) = s
+TensorIndex(name::AbstractString,tensor_index_type::TensorIndexType,is_up::Bool=true)::TensorIndex = tensor.TensorIndex(name,tensor_index_type,is_up)
+TensorIndex(name::Symbol,tensor_index_type::TensorIndexType,is_up::Bool=true)::TensorIndex = tensor.TensorIndex(name,tensor_index_type,is_up)
+
+-( x::TensorIndex ) = TensorIndex(x.__neg__())
+
+TensorHead(s::TensorHead) = s
+TensorHead(name::AbstractString,index_types::AbstractArray{TensorIndexType},symmetry::TensorSymmetry)::TensorHead = tensor.TensorHead(name,index_types,symmetry)
+TensorHead(name::AbstractString,index_types::AbstractArray{TensorIndexType})::TensorHead = tensor.TensorHead(name,index_types)
+TensorHead(name::Symbol,index_types::AbstractArray{TensorIndexType},symmetry::TensorSymmetry)::TensorHead = tensor.TensorHead(name,index_types,symmetry)
+TensorHead(name::Symbol,index_types::AbstractArray{TensorIndexType})::TensorHead = tensor.TensorHead(name,index_types)
+
+TensorIndexType(name::AbstractString,dummy_name::AbstractString,dim::Int,metric_name::AbstractString)::TensorIndexType = tensor.TensorIndexType(name,dummy_name,dim,metric_name)
+TensorIndexType(name::AbstractString,dummy_fmt::AbstractString,dim::Int)::TensorIndexType = tensor.TensorIndexType(name,dummy_fmt=dummy_fmt,dim=dim)
+TensorIndexType(name::AbstractString,dummy_fmt::AbstractString)::TensorIndexType = tensor.TensorIndexType(name,dummy_fmt=dummy_fmt)
+TensorIndexType(name::Symbol,dummy_name::Symbol,dim::Int,metric_name::Symbol)::TensorIndexType = tensor.TensorIndexType(string(name),string(dummy_name),dim,string(metric_name))
 
 macro heads(TIT::TensorIndexType,x...)
 
