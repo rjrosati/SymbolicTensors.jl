fcce2f15434e6f1d9a9a6a7795cf88d8ef5d3615
Robbie Rosati <rjrosati@utexas.edu>
2020-05-11 17:52:52 -0500

    now can index tensors properly

--- src/SymbolicTensors.jl
+++ src/SymbolicTensors.jl
@@ -52,14 +52,28 @@ TensorHead(name::AbstractString,index_types::AbstractArray{TensorIndexType},symm
 TensorHead(name::AbstractString,index_types::AbstractArray{TensorIndexType})::TensorHead = tensor.TensorHead(name,index_types)
 TensorHead(name::Symbol,index_types::AbstractArray{TensorIndexType},symmetry::TensorSymmetry)::TensorHead = tensor.TensorHead(name,index_types,symmetry)
 TensorHead(name::Symbol,index_types::AbstractArray{TensorIndexType})::TensorHead = tensor.TensorHead(name,index_types)
+(t::TensorHead)(ics::TensorIndex...) = t.__pyobject__(ics)
 
-TensorIndexType(name::AbstractString,dummy_name::AbstractString,dim::Int,metric_name::AbstractString)::TensorIndexType = tensor.TensorIndexType(name,dummy_name,dim,metric_name)
+
+TensorIndexType(name::AbstractString,dummy_fmt::AbstractString,dim::Int,metric_name::AbstractString)::TensorIndexType = tensor.TensorIndexType(name,dummy_fmt=dummy_fmt,dim,metric_name)
 TensorIndexType(name::AbstractString,dummy_fmt::AbstractString,dim::Int)::TensorIndexType = tensor.TensorIndexType(name,dummy_fmt=dummy_fmt,dim=dim)
 TensorIndexType(name::AbstractString,dummy_fmt::AbstractString)::TensorIndexType = tensor.TensorIndexType(name,dummy_fmt=dummy_fmt)
 TensorIndexType(name::Symbol,dummy_name::Symbol,dim::Int,metric_name::Symbol)::TensorIndexType = tensor.TensorIndexType(string(name),string(dummy_name),dim,string(metric_name))
 
-macro heads(TIT::TensorIndexType,x...)
 
+macro heads(IT::AbstractArray{TensorIndexType},xs...)
+    hs = []
+    for x in xs
+        push!(hs,TensorHead(x,IT))
+    end
+    return hs
+end
+macro heads(IT::AbstractArray{TensorIndexType},xs...)
+    hs = []
+    for x in xs
+        push!(hs,TensorHead(x,IT))
+    end
+    return hs
 end
 
 macro indices(TIT::TensorIndexType,x...)
